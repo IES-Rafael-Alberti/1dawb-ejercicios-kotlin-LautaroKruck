f1da0563ff4bcc2c295a252f8a07d3e1a0983310
Lautaro <lkru144@g.educaand.es>
2024-01-09 13:11:26 +0100

    a

--- TABLAS/src/main/kotlin/Main.kt
+++ TABLAS/src/main/kotlin/Main.kt
@@ -1,32 +1,15 @@
 fun main() {
-
-    var tabla: Int
-
     do {
+        val num = pedirNum(1, 100)
+        val tabla = Array(10) { i -> (i + 1) * num }
 
-        limpiaConsola()
-        mostrarMenuPpal()
-        tabla = pedirtabla(1, 100)
+        println("Tabla de multiplicar del $num:")
+        for (i in 0 until tabla.size) {
+            println("$num x ${i + 1} = ${tabla[i]}")
+        }
 
-
-    } while (tabla >= 1 and tabla <= 100)
-
+    } while (pregunta("¿Desea generar otra tabla de multiplicación?"))
 }
-
-
-fun mostrarMenuPpal() {
-    println("Introduzca un número entre 1 y 100:")
-}
-
-fun tabla(){
-    limpiaConsola()
-
-    val allCards = Array(12, { i -> i + 1 })
-    println("${allCards.first()} - ${allCards.last()}") // => 1 - 12
-
-
-}
-
 fun pedirtabla(min: Int, max: Int): Int {
 
     var tabla: Int
@@ -50,17 +33,41 @@ fun pedirtabla(min: Int, max: Int): Int {
     return tabla
 }
 
-fun mensajeError(id: Int) {
-    when (id) {
-        1 -> println("**Error** Número no válido (pulse ENTER para continuar...)")
-        else -> {
-            println("**Error** Número no válido (pulse ENTER para continuar...)")
-        }
-    }
-}
-
 fun limpiaConsola() {
     for (i in 1..20) {
         println()
     }
 }
+import java.util.*
+
+fun pedirNum(min: Int, max: Int): Int {
+    var num: Int
+    val scanner = Scanner(System.`in`)
+    while (true) {
+        try {
+            print("Introduce un número entre $min y $max: ")
+            num = scanner.nextInt()
+            if (num in min..max) {
+                break
+            } else {
+                println("Número no válido. Debe estar en el rango especificado.")
+            }
+        } catch (e: InputMismatchException) {
+            println("Error en entrada. Debes introducir un número entero.")
+            scanner.nextLine() // Limpiar el buffer del scanner
+        }
+    }
+    return num
+}
+
+fun pregunta(text: String): Boolean {
+    val scanner = Scanner(System.`in`)
+    while (true) {
+        print("$text (s/n): ")
+        val respuesta = scanner.next().toLowerCase()
+        if (respuesta == "s") return true
+        else if (respuesta == "n") return false
+        else println("Error en respuesta. Debes contestar con 's' o 'n'.")
+    }
+}
+
